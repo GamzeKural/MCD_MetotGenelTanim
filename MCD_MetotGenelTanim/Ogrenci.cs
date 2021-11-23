c66abda20e4655b3b1d83583e833dbeedf827b76
103AKSAM_GAMZE <103AKSAM_GAMZE@DESKTOP-TUMHS1A>
2021-11-23 16:40:34 +0300

    Add project files.

---
+++ MCD_MetotGenelTanim/Ogrenci.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_MetotGenelTanim
+{
+    public class Ogrenci
+    {
+        public void selamlaOgrenci() 
+        {
+            Console.WriteLine("Merhaba Öğrenci");
+
+        }
+
+        private void ogrenciMetot1(string ogrenciAdi, string ogrenciSoyadi)
+        {
+            Console.WriteLine("Öğrenci bilgileri : {0} {1}",ogrenciAdi,ogrenciSoyadi);
+        }
+
+        public void ogrenciMetot2()
+        {
+            ogrenciMetot1("Serdar", "Karakurt");
+
+        }
+
+        public string adGetir()
+        {
+            return " ";
+        }
+
+        
+    }
+
+    /*
+     * [ErişimBelirleyicisi] <Geriye dönüş değeri> MetodAdi(Parametreler)
+     *{
+     *   //Metod için gereken kodlar
+     *}
+     *Public
+     *Private
+     *Protected
+     *Internal
+     *Internal Protected
+     */
+
+
+}
---
+++ MCD_MetotGenelTanim/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_MetotGenelTanim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba");
+            selamla();
+            Ogrenci o1 = new Ogrenci();
+            o1.ogrenciMetot2();
+            o1.selamlaOgrenci();
+
+            Ogrenci o2 = new Ogrenci();
+            o2.ogrenciMetot2();
+            o2.selamlaOgrenci();
+
+
+            Console.ReadLine();
+        }
+
+        static void selamla()
+        {
+            Console.WriteLine("Merhaba");
+        }
+    }
+}
